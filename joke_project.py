366e1bb82ecfbd65159c48e41a053bc279c2a4a7
oritzau <114698512+oritzau@users.noreply.github.com>
2022-10-04 10:33:56 -0400

    Update joke_project.py

--- joke_project.py
+++ joke_project.py
@@ -1,29 +1,25 @@
-import time
 import os
 os.system('color')
 
 def joke_deliver(setup, punchline):
     bold = 'a'
-    space = '\n'
     while bold.lower() != 'y' or bold.lower() != 'n':
         underline = input('Underline? (Y / N) ')
         if underline.lower() == 'y':
             find_string = input('What words?: ')
             under_string = ('\033[4m' + find_string + '\033[0m')
             punchline_w_under = punchline.replace(find_string, under_string)
-            delay = float(input('Delay: '))
-            print(50 * '\n')
-            time.sleep(1.25)
+            os.system('cls')
             print(setup + '\n\n')
-            time.sleep(delay)
+            nothing_var = input('Press Enter to view ')
+            os.system('cls')
             print(punchline_w_under)
             break
         if underline.lower() == 'n':
-            delay = float(input('Delay: '))
-            print(50 * '\n')
-            time.sleep(1.25)
-            print(50 * '\n' + setup + '\n\n')
-            time.sleep(delay)
+            os.system('cls')
+            print(setup + '\n\n')
+            nothing_var = input('Press Enter to view ')
+            os.system('cls')
             print(punchline)
             break
 if __name__ == '__main__':
